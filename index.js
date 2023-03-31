f4838ecd4577f4dd8e169d984c8d6e7ebf080047
AkashChakrabortty <akashchakrabortty2000@gmail.com>
2023-03-31 17:50:56 +0600

    course add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,25 +11,28 @@ app.use(express.json());
 
 // mongo
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kusbv.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-
-         
-
-    }
-
-    catch{
-
-    }
+async function run() {
+  const courseCollection = client.db("task").collection("courses");
+  try {
+    //insert course into the database
+    app.post("/add", async (req, res) => {
+      const info = req.body;
+      const result = await courseCollection.insertOne(info);
+      res.send(result);
+    });
+  } catch {}
 }
-run().catch(err => console.log(err))
+run().catch((err) => console.log(err));
 
-
-app.get('/', (req,res)=>{
-    res.send('api found')
-})
-app.listen(port, ()=>{
-    console.log('server running')
-})
+app.get("/", (req, res) => {
+  res.send("api found");
+});
+app.listen(port, () => {
+  console.log("server running");
+});
